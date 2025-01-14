cbd8c879646afb8554c2581708af0abe3244f382
samuel <samawyck@gmail.com>
2025-01-14 13:19:51 -0600

    Add PostFixEval object

--- calculator.js
+++ calculator.js
@@ -355,3 +355,158 @@ function ShuntingConversion() {
         }
     }
 }  
+
+
+
+function PostFixEval() {
+    this.operators = new Set(
+        ["+", "-", "*", "/", "^"]
+    );
+
+    this.functions = new Set(
+        ["L", "S", "C", "T"]
+    );
+
+    this.degrees = true;
+
+
+
+    this.evaluate = function(expression, degrees=true) {
+        this.degrees = degrees;
+        let operandStack = [];
+        const expressionSize = expression.length;
+        let pointer = 0;
+
+        while (pointer < expressionSize) {
+            let currentElement = expression[pointer];
+
+            if (this.operators.has(currentElement)) {
+                this.evalOperation(currentElement, operandStack);
+
+            } else if (this.functions.has(currentElement)) {
+                this.evalFunction(currentElement, operandStack);
+
+            } else {
+                operandStack.push(currentElement);
+            }
+            pointer += 1;
+        }
+
+        return operandStack;
+    }
+
+
+    this.evalFunction = function(functionSymbol, operandStack) {
+        if (functionSymbol === "L") {
+            this.handleLog(operandStack);
+        } else if (functionSymbol === "S") {
+            this.handleSine(operandStack);
+        } else if (functionSymbol === "C") {
+            this.handleCosine(operandStack);
+        } else if (functionSymbol === "T") {
+            this.handleTangent(operandStack);
+        }
+    }
+
+
+    this.toRadians = function(angle) {
+        return angle * (Math.PI / 180);
+    }
+
+
+    this.handleCosine = function(operandStack) {
+        let angle = Number(operandStack.pop());
+        if (this.degrees) {
+            angle = this.toRadians(angle);
+        }
+        let result = Math.cos(angle);
+        operandStack.push(result);
+    }
+
+
+    this.handleLog = function(operandStack) {
+        let operand = Number(operandStack.pop());
+        let result = Math.log10(operand);
+        operandStack.push(result);
+    }
+
+
+    this.handleSine = function(operandStack) {
+        let angle = Number(operandStack.pop());
+        if (this.degrees) {
+            angle = this.toRadians(angle);
+        }
+        let result = Math.sin(angle);
+        operandStack.push(result);
+    }
+
+
+    this.handleTangent = function(operandStack) {
+        let angle = Number(operandStack.pop());
+        if (this.degrees) {
+            angle = this.toRadians(angle);
+        }
+        let result = Math.tan(angle);
+        operandStack.push(result);
+    }
+
+
+    this.evalOperation = function(operator, operandStack) {
+        if (operator === "+") {
+            this.handleAddition(operandStack);
+        } else if (operator === "-") {
+            this.handleSubtraction(operandStack);
+        } else if (operator === "*") {
+            this.handleMultiplication(operandStack);
+        } else if (operator === "/") {
+            this.handleDivision(operandStack);
+        } else if (operator === "^") {
+            this.handleExponent(operandStack);
+        }
+    }
+
+
+    this.handleAddition = function(operandStack) {
+        let rightSide = Number(operandStack.pop());
+        let leftSide = Number(operandStack.pop());
+
+        let result = leftSide + rightSide;
+        operandStack.push(result);
+    }
+
+
+    this.handleSubtraction = function(operandStack) {
+        let rightSide = Number(operandStack.pop());
+        let leftSide = Number(operandStack.pop());
+
+        let result = leftSide - rightSide;
+        operandStack.push(result);
+    }
+
+
+    this.handleMultiplication = function(operandStack) {
+        let rightSide = Number(operandStack.pop());
+        let leftSide = Number(operandStack.pop());
+
+        let result = leftSide * rightSide;
+        operandStack.push(result);
+    }
+
+
+    this.handleDivision = function(operandStack) {
+        let rightSide = Number(operandStack.pop());
+        let leftSide = Number(operandStack.pop());
+
+        let result = leftSide / rightSide;
+        operandStack.push(result);
+    }
+
+
+    this.handleExponent = function(operandStack) {
+        let rightSide = Number(operandStack.pop());
+        let leftSide = Number(operandStack.pop());
+
+        let result = Math.pow(leftSide, rightSide);
+        operandStack.push(result);
+    }
+}
